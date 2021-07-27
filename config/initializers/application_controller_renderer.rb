8434146a50da4b2edc665fa6253dde3e0cd1d17e
SatoJura <jnnsk0snb@gmail.com>
2021-07-27 11:42:36 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ba0863cc79baaff2d0b8602b5d1800fbee87d216d5cd0074183a064b5abd8ed93bcaa163fabd715e98ee1f4d087801ba4c20cae4c09f92993cb8321c96143dcf'
