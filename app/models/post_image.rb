9492d6b45ac41a71bc14b764b5939eedd13dde11
SatoJura <jnnsk0snb@gmail.com>
2021-07-28 09:17:02 +0000

    [finish]4-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,4 +2,11 @@ class PostImage < ApplicationRecord
   
   belongs_to :user
   attachment :image
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  def favorited_by?(a)
+    favorites.where(user_id: a.id).exists?
+  end
+
 end
